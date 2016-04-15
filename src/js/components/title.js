e459f9e42dd23756b107814966f42a62cda8aba8
神奇辉 <shenqihui0920@gmail.com>
2016-04-15 09:55:31 +0800

    ~

--- src/js/components/title.js
+++ src/js/components/title.js
@@ -2,10 +2,20 @@
 
 var React = require('react');
 
+var FlipMove = require('react-flip-move');
+
 var Title = React.createClass({
+  renderTopArticles: function() {
+    return <span>Hello world</span>
+  },
   render: function() {
     return <p>
       {this.props.name}
+      <div className="top-articles">
+        <FlipMove easing="cubic-bezier(0, 0.7, 0.8, 0.1)">
+          { this.renderTopArticles() }
+        </FlipMove>
+      </div>
     </p>;
   }
 });
